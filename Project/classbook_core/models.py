ca6d6b406aac87cbd6a1d3b26de208a854b2f6b0
Leon Rabinovich <Leon.Rabinovich@Safend.com>
2021-07-27 12:24:58 +0300

    Adding comment model

--- Project/classbook_core/models.py
+++ Project/classbook_core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -56,4 +57,10 @@ class Document(models.Model):
     rating = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(5)])
     upload_date = models.DateField(auto_now_add=True)
 
-
+class Comment(models.Model):
+    associated_document = models.ForeignKey(Document, on_delete=CASCADE)
+    auther = models.ForeignKey(Profile, on_delete=CASCADE)
+    content = models.TextField(max_length=500, validators=[MinValueValidator(1)])
+    publish_date = models.DateField(auto_now_add=True)
+    replied_to_comment = models.OneToOneField('Comment', on_delete=CASCADE)
+    likes_count = models.IntegerField(default = 0)
