4969c7e19b5315df849d24e4b06a39d520aa1ff9
Leon Rabinovich <Leon.Rabinovich@Safend.com>
2021-07-29 12:11:17 +0300

    Improve the design of the sign-in and sign-up pages

--- Project/classbook/settings.py
+++ Project/classbook/settings.py
@@ -45,6 +45,15 @@ INSTALLED_APPS = [
     'crispy_forms',
 ]
 
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
