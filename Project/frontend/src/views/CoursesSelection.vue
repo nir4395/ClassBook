7fb495e009f33afbac138de08009d9e5edaaa3e1
nir4395 <77625082+nir4395@users.noreply.github.com>
2021-08-03 18:44:19 +0300

    Ui Update regrading Views folder(Pages):      -Add courses selection view ,that also contains filters bar for the          different categories(Exams,summaries,Homework). the view has 	 basic 	     support of reterving data from the server (via axios POST 	     requst). 	         funtionallty is not yet complete.
    		     -Added code for school selection and categories
    		     pages , to navagite to
    		         the courses selection using the global router
    			 object (progrmaticly each
    			     click leads to the page with differnt
    			     loaded data)
    
    			         -Comments section was added to the
    				 Document preivew page in the bottom.
    				     design is still not complete.
    
    				         -Minimal changes were done to
    					 home page in design,still not
    					 complete
    
    					     -Changed design of
    					     Docuemnts Selection page to
    					     support vutify
    					         -Changed design of
    						 Docuemnts Selection
    						 page to support vutify
    						     docuemnts without
    						     gallery(removed).

---
+++ Project/frontend/src/views/CoursesSelection.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+   
+        <CourseViewer :courses="info"></CourseViewer>
+    </div>
+    
+</template>
+
+<script>
+import CourseViewer from '../components/Courses/CoursesViewer.vue'
+import axios from 'axios'
+axios.defaults.xsrfHeaderName = "X-CSRFTOKEN"
+axios.defaults.xsrfCookieName = "csrftoken";
+export default {
+
+     data(){
+        return{
+            category:this.$route.params.category,
+            info:[{name:'Calculus 1',lecturer:'Romina Zigdon',rank:'2',courseStage:'A',school:'CS',id:1},
+            {name:'Linear Algebra 1',lecturer:'Dan Levy',rank:'5',courseStage:'A',school:'CS',id:2},
+            {name:'Introduction to CS 1',lecturer:'Hadar Binsky',rank:'5',courseStage:'A',school:'CS',id:3} ]
+                
+        }
+    },
+
+    components:{
+        CourseViewer
+    },
+    methods:{
+        getCourses(){
+            
+            //course/get/ins=1,year='+this.category
+        let url='http://localhost:8000/course/get/course_id=2/cat=2'
+        console.log(url)
+                 let headers = {
+         "X-CSRFToken": axios.defaults.xsrfCookieName,
+         "Content-Type": "application/x-www-form-urlencoded"
+       }
+              axios
+     
+    .get(url,headers)
+    .then(response => (this.info = response.data.courses_by_year))
+        },
+               
+    },
+    created(){
+        //this.getCourses()
+    }
+}
+   
+</script>
+<style scoped>
+
+</style>
---
+++ Project/frontend/src/views/MyCourses.vue
@@ -0,0 +1,11 @@
+<template>
+    <CourseViewer></CourseViewer>
+</template>
+<script>
+ import CourseViewer from '../components/Courses/CoursesViewer.vue'
+export default {
+   componenets:{
+       CourseViewer
+   }
+}
+</script>
