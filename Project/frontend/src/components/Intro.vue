192f25370062fb23a66f0c5f757f66398cf84723
nir4395 <77625082+nir4395@users.noreply.github.com>
2021-08-15 16:48:28 +0300

    untracked files on main: be98647 Merge pull request #52 from nir4395/commentFixBug

---
+++ Project/frontend/src/components/Intro.vue
@@ -0,0 +1,73 @@
+<template>
+     <v-container  >
+      <v-row  class="mx-auto" style="height:600px;width:1000px">
+          <div
+            class="image"
+            
+  
+            :style="{ backgroundImage: 'url(' + require(`@/assets/${image}`) + ')', width: '100%', height: '100%' }"
+>
+        <img src="../assets/Covers/large_studentsmarts.png" style="height:40px;width:40px">
+        <h1  class="mx-auto" style="color:black;margin-top:10px">All Your Study Materials in one place</h1>
+        <p  class="mx-auto lead" style="color:black">
+  View,Upload Share and rate your course in few clicks a way
+</p>
+</div>
+        </v-row >
+       <v-row  class="mx-auto homePageRow">   
+             <v-col><h3>Discover & Enjoy our Key features</h3></v-col>
+                      
+        </v-row >
+         <v-row  class="mx-auto homePageRow" >
+           
+              <v-col><v-icon color="black" class="circle-icon">school</v-icon></v-col>
+               <v-col>View Study Materials</v-col>
+           
+            
+              <v-col><v-icon  color="#F5BB00" class="circle-icon">star</v-icon></v-col>
+               <v-col>Rate Documents</v-col>
+         
+           
+            
+        </v-row >
+         <v-row  class="mx-auto homePageRow" >
+           
+              <v-col ><v-icon color="blue" class="circle-icon">upload_file</v-icon></v-col>
+               <v-col>Upload Files</v-col>
+           
+            
+              <v-col><v-icon  color="black" class="circle-icon">question_answer</v-icon></v-col>
+               <v-col>Discuss with your friends</v-col>
+         
+          
+            
+        </v-row >
+   </v-container>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            image : 
+          "Covers/campus2.jpg"
+        }
+    }
+}
+</script>
+<style scoped>
+.homePageRow{
+  height:100px;width:1000px;background-color:white
+}
+.image{
+  padding:14%;
+  box-shadow: 100px;
+  
+}
+.circle-icon {
+    background: lightgray;
+    padding:30px;
+    border-radius: 50%;
+    height:50px;
+    width:50px
+}
+</style>
---
+++ Project/frontend/src/components/ProfileDetails.vue
@@ -0,0 +1,100 @@
+<template>
+     <div class="panel-body bio-graph-info">
+              <h1>Personal Information</h1>
+              <div class="row">
+                  <div class="bio-row">
+                      <p><span>First Name </span>: Marcus</p>
+                  </div>
+                  <div class="bio-row">
+                      <p><span>Last Name </span>: Obrien</p>
+                  </div>
+                  <div class="bio-row">
+                      <p><span>Country </span>: Israel</p>
+                  </div>
+                  <div class="bio-row">
+                      <p><span>Member Since</span>: 21 July 2020</p>
+                  </div>
+                
+                  <div class="bio-row">
+                      <p><span>Email </span>: marcu2196@mta.ac.il</p>
+                  </div>
+                   <div class="bio-row">
+                      <p><span>School </span>: Computer Siecnce</p>
+                  </div>
+                  <div class="bio-row">
+                      <p><span>Year </span>: Third</p>
+                  </div>
+                
+               
+              </div>
+          </div>
+</template>
+<style scoped>
+.bio-graph-heading {
+    background: #fbc02d;
+    color: #fff;
+    text-align: center;
+    font-style: italic;
+    padding: 40px 110px;
+    border-radius: 4px 4px 0 0;
+    -webkit-border-radius: 4px 4px 0 0;
+    font-size: 16px;
+    font-weight: 300;
+}
+
+.bio-graph-info {
+    color: #89817e;
+}
+
+.bio-graph-info h1 {
+    font-size: 22px;
+    font-weight: 300;
+    margin: 0 0 20px;
+}
+
+.bio-row {
+    width: 50%;
+    float: left;
+    margin-bottom: 10px;
+    padding:0 15px;
+}
+
+.bio-row p span {
+    width: 100px;
+    display: inline-block;
+}
+
+.bio-chart, .bio-desk {
+    float: left;
+}
+
+.bio-chart {
+    width: 40%;
+}
+
+.bio-desk {
+    width: 60%;
+}
+
+.bio-desk h4 {
+    font-size: 15px;
+    font-weight:400;
+}
+
+.bio-desk h4.terques {
+    color: #4CC5CD;
+}
+
+.bio-desk h4.red {
+    color: #e26b7f;
+}
+
+.bio-desk h4.green {
+    color: #97be4b;
+}
+
+.bio-desk h4.purple {
+    color: #caa3da;
+}
+
+</style>
