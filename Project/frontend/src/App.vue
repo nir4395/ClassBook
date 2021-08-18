a390d502eded8011c66a12dbdaf0d4bb898b45d2
nir4395 <77625082+nir4395@users.noreply.github.com>
2021-08-18 10:34:30 +0300

    main compoenent App.vue was updated with  background image and fonts changes, it applies  for all sub components

--- Project/frontend/src/App.vue
+++ Project/frontend/src/App.vue
@@ -1,43 +1,37 @@
-<template >
-
-  <v-app  class="gray fontfam">
-    <h1>teeext</h1>
-  <span class="bg"></span>
- 
+<template>
+  <v-app class="gray fontfam">
+    <span class="bg"></span>
     <Navbar></Navbar>
-    <!-- <pdf src="..\assets\comp1.pdf"></pdf> -->
-    
-  <!-- <v-btn blue>GO TO SIGN UP</v-btn> -->
-    <v-content >
-      <router-view ></router-view>
+    <v-content>
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import Navbar  from '@/components/Navbar'
+import Navbar from "@/components/Navbar";
 export default {
-  name: 'App',
-  components:{Navbar},
+  name: "App",
+  components: { Navbar },
   data: () => ({
-    image2: "Covers/back2.jpg"
+    image2: "Covers/back2.jpg",
     //
   }),
 };
 </script>
 <style scoped>
-.bg{
-   width: 100%;
-    height: 100%;
-    position: absolute;
-    top: 0;
-    left: 0;
-    background: url( 'assets/Covers/back5.jpg') no-repeat center center;
-    background-size: cover;
-    /* background-color: gray; */
-    transform: scale(1.1);
+.bg {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: url("assets/Covers/back5.jpg") no-repeat center center;
+  background-size: cover;
+  /* background-color: gray; */
+  transform: scale(1.1);
 }
-.fontfam{
-  font-family: 'Rubik', sans-serif;
+.fontfam {
+  font-family: "Rubik", sans-serif;
 }
 </style>
