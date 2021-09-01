40bcdf3063d80a56cc86bc769ccd7cac9bda6301
nir4395 <77625082+nir4395@users.noreply.github.com>
2021-09-01 20:36:54 +0300

    updated App.vue file to pass props to navbar component, thus navbar shows the username of the user and his profile picture in the top of the bar

--- Project/frontend/src/App.vue
+++ Project/frontend/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <v-app class="gray fontfam">
     <span class="bg"></span>
-    <Navbar></Navbar>
+    {{uerData}}
+    <Navbar :name="uerData.profile_details.user.username"></Navbar>
     <v-content>
       <router-view></router-view>
     </v-content>
@@ -13,7 +14,35 @@ import Navbar from "@/components/Navbar";
 export default {
   name: "App",
   components: { Navbar },
+  created(){
+    this.getUsersInfo()
+  },
+  methods:{
+    async getUsersInfo() {
+      try {
+        console.log("ask for the data in App.vue")
+        var url = "http://localhost:8000/users/user_profile/";
+        const response = await this.$http.get(url);
+        console.log(response)
+        this.uerData = response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async getUsersCoursesInfo() {
+      try {
+        var url = "http://localhost:8000/course/user_registered/";
+
+        const response = await this.$http.get(url);
+      this.registeredCourses = response.data["registered_courses"];
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
   data: () => ({
+    uerData:'',
+    registeredCourses:'',
     image2: "Covers/back2.jpg",
     //
   }),
